a0880e0b1c3d48eba4558cfdbbbf9fc363c8b39b
南烁 <nanshuo.icu@qq.com>
2024-07-07 11:13:54 +0800

    删除PostController帖子接口里使用的aop参数的权限校验

--- src/main/java/com/nanshuo/project/model/dto/post/PostAddRequest.java
+++ src/main/java/com/nanshuo/project/model/dto/post/PostAddRequest.java
@@ -1,7 +1,5 @@
 package com.nanshuo.project.model.dto.post;
 
-import com.nanshuo.project.annotation.CheckParam;
-import com.nanshuo.project.constant.NumberConstant;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -19,19 +17,16 @@ public class PostAddRequest implements Serializable {
     /**
      * 标题
      */
-    @CheckParam(alias = "标题", maxLength = 80)
     private String title;
 
     /**
      * 内容
      */
-    @CheckParam(alias = "内容", maxLength = 8192)
     private String content;
 
     /**
      * 标签列表
      */
-    @CheckParam(alias = "标签", required = NumberConstant.FALSE_ZERO_VALUE)
     private List<String> tags;
 
     private static final long serialVersionUID = 1L;
