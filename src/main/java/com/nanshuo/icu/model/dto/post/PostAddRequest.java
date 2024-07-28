4b010992b2dc54bbe5eb61034812e3091cd6fe96
南烁 <nanshuo.icu@qq.com>
2024-07-28 10:43:37 +0800

    整体优化了一下整个项目，包括项目结构、代码等等，添加了一个超级管理员身份，最高权限身份

--- src/main/java/com/nanshuo/icu/model/dto/post/PostAddRequest.java
+++ src/main/java/com/nanshuo/icu/model/dto/post/PostAddRequest.java
@@ -1,5 +1,7 @@
 package com.nanshuo.icu.model.dto.post;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
